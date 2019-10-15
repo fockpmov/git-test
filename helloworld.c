744577779d0650ff85b891e4041c8370af2a02d9
fockpmov <fockpmov@gmail.com>
2019-10-15 21:44:38 +0800

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,4 @@
-/*************************************************************************
-	> File Name: helloworld.c
-	> Author: fockpmov
-	> Mail: fockpmov@gmail.com 
-	> Created Time: Tue 15 Oct 2019 09:39:55 PM CST
-************************************************************************/
+
 
 #include<stdio.h>
 int main(void)
