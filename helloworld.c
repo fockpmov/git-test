32ab032db160aac0d2e6f45789865c853a237f9a
fockpmov <fockpmov@gmail.com>
2019-10-15 21:41:36 +0800

    Add helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: helloworld.c
+	> Author: fockpmov
+	> Mail: fockpmov@gmail.com 
+	> Created Time: Tue 15 Oct 2019 09:39:55 PM CST
+************************************************************************/
+
+#include<stdio.h>
+int main(void)
+{
+	printf("Hello world!\n");
+	return 0;
+}
